0dc179a966baf213066f957b2a26c52e58a7fe57
Aaron Taylor <ajtaylor73@gmail.com>
2022-10-04 21:58:10 -0500

    Add Log class

--- whatido.py
+++ whatido.py
@@ -1,13 +1,28 @@
 # whatido.py
 
+
+class Log:
+	DEFAULT_LOG = "captains.log"
+
+	def __init__(self):
+		self.filepath = "."
+		self.filename = Log.DEFAULT_LOG
+
+	def store(self, log_item):
+		print(f"Recording: {log_item}")
+
+
+
 def get_user_msg():
 	msg = input("--> ")
 	if msg == 'q':
 		exit()
-	print(f"Recording: {msg}")
 	return msg
 	
 if __name__ == '__main__':
+	log = Log()
+
 	while (True):
-		get_user_msg()
+		log_item = get_user_msg()
+		log.store(log_item)
 
